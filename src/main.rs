340d1d22cb3ca6a5727187a2f3562e35f0637034
Caleb Meyer <Kiaulen@gmail.com>
2018-08-13 09:08:02 -0500

    Fixed version (thanks @newpavlov!)

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,47 @@
+#[derive(Copy, Clone, Debug)]
+enum Rank {
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace,
+}
+
+#[derive(Copy, Clone, Debug)]
+enum Suit {
+    Spades,
+    Hearts,
+    Clubs,
+    Diamonds,
+}
+
+#[derive(Copy, Clone, Debug)]
+struct Card {
+    rank: Rank,
+    suit: Suit,
+}
+
 fn main() {
-    enum Rank {
-        Two,
-        Three,
-        Four,
-        Five,
-        Six,
-        Seven,
-        Eight,
-        Nine,
-        Ten,
-        Jack,
-        Queen,
-        King,
-        Ace,
-    }
     let ranks = [
         Rank::Two, Rank::Three, Rank::Four, Rank::Five, Rank::Six, Rank::Seven,
         Rank::Eight, Rank::Nine, Rank::Ten, Rank::Jack, Rank::Queen, Rank::King,
         Rank::Ace,
     ];
 
-    enum Suit {
-        Spades,
-        Hearts,
-        Clubs,
-        Diamonds,
-    }
     let suits = [Suit::Spades, Suit::Hearts, Suit::Clubs, Suit::Diamonds,];
-
-    struct Card {
-        rank: Rank,
-        suit: Suit,
-    }
 
     let mut deck: Vec<Card> = Vec::new();
 
-    for suit in suits.iter() {
-        for rank in ranks.iter() {
+    for &suit in suits.iter() {
+        for &rank in ranks.iter() {
             deck.push(Card{rank, suit});
         }
     }
